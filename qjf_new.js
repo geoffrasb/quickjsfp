b182eed8248e7607132cfad575ce7207fe8e8bfa
Geoffr Su <geoffrasb@gmail.com>
2016-11-10 17:08:22 +0800

    data types for type and pattern

--- qjf_new.js
+++ qjf_new.js
@@ -25,35 +25,58 @@ function uName(n){
 function DontCare(){}
 
 function Tuple(n,items){
-  checkType(n,Number,'n','Tuple');
+  checkType(n,Number,'n','Tuple(internal)');
   checkType(items,Array,'items','Tuple');
   checkValue(items.length,n,'items.length','Tuple');
 
   this.items = items;
 }
 
+function List(items){
+  checkType(items,Array,'items','List(internal)');
+  this.items = items;
+}
+
 //----type
 
 function ImplicitType(n,t){
+  checkType(n,Name,'n','ImplicitType');
+  //t : Type
 
   this.name = n;
   this.type = t;
 }
 function ArrowType(t1,t2){
-  this.left = t1;
-  this.right = t2;
+  //t1,t2 : Type
+  this.lefttype = t1;
+  this.righttype = t2;
 }
 function SubType(t){
-  this.type = t;
+  //t : Type
+  this.subtype = t;
 }
 function SupType(t){
-  this.type = t;
+  //t : Type
+  this.suptype = t;
 }
-function RecordType(obj){
-  this.record = obj;
+function RecordType(keytypes){
+  //keytypes : [['k1', type], ['k2', type] ...]
+
+  this.keytypes = keytypes;
 }
 function ListType(t){
-  this.type = t;
+  //t : Type
+  this.listype = t;
+}
+
+function RealType(introform){
+  //introform : [Constructor, arg1, arg2...]
+  this.introform = introform;
+}
+
+function TypeVar(varname){
+  checkType(varname,String,'varname','TypeVar');
+  this.varname = varname;
 }
 
 function Type(t){
@@ -64,33 +87,85 @@ function Type(t){
     || t.constructor === Tuple
     || t.constructor === RecordType
     || t.constructor === ListType
-    || t.constructor === Name
+    || t.constructor === RealType
+    || t.constructor === TypeVar
   ){
-    this.kind = t.constructor;
     this.type = t;
   }else{
-    console.log('Error: new Type: invalid type kind');
+    console.log('type error: new Type: invalid type kind');
   }
 } 
 
+//------- declarations
 
-//------- pattern
+
+//------- patterns
 
 function IntroForm(cnstr, patterns){
+  checkType(patterns,Array,'patterns','IntroForm');
+  //[IPattern]
+
+  this.cnstr = cnstr;
+  this.patterns = patterns;
 }
+function RecordPattern(leadvar, restvar, keyvals){
+  checkType(leadvar,String,'leadvar','RecordPattern');
+  checkType(restvar,String,'restvar','RecordPattern');
+  checkType(keyvals,Array,'keyvals','RecordPattern');
+  //[['k1',IPattern], ['k1',IPattern]]
+
+  this.leadvar = leadvar; //"" for case of no leadvar
+  this.restvar = restvar; //"" for case of no restvar
+  this.keyvals = keyvals;
+}
+function ConsPattern(x,xs){
+  //x : IPattern
+  //xs : IPattern
+
+  this.head = x;
+  this.tail = xs;
+}
+function NilPattern(){}
 
 function IPattern(inp){
-  switch(inp){
+  switch(inp.constructor){
     case DontCare:
     case Name:
     case IntroForm:
     case Tuple:
+    case List:
+    case IPattern:
+    case RecordPattern:
+    case ConsPattern:
+    case NilPattern:
+      this.ipattern = inp;
+      break;
+    default:
+      throw "error at IPattern"
   }
 }
 
-function CPattern
+function CPattern(observer, patterns){
+  if(observer.constructor !== DontCare
+    && observer.constructor !== Name)
+    throw "cpattern error"
+  checkType(patterns,Array,'patterns','CPattern');
+  //patterns : [IPattern]
 
-function WholePattern
+  this.observer = observer;
+  this.patterns = patterns;
+}
+
+function WholePattern(inp){
+  switch(inp.constructor){
+    case Array:
+    case CPattern:
+      this.wholepattern = inp;
+      break;
+    default:
+      throw "error at WholePattern"
+  }
+}
 
 //------- Q.J.F. API
 // module,record,data,codata,func,REC,case; literal expression, pattern assignment
